849f0ebd71e1e65e2b1f6964b537c40d82d6659e
Vini Soto <18271663+vinisoto@users.noreply.github.com>
2019-07-10 11:53:44 -0700

    Add project files.

---
+++ AppSettings/Default.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AppSettings._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="row">
+        <div class="col-xs-12">
+            <h2>App Settings:</h2>
+            <p>
+                <asp:Literal ID="ltAppSettings" runat="server"></asp:Literal>
+            </p>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ AppSettings/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AppSettings
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            foreach (string key in ConfigurationManager.AppSettings.AllKeys)
+            {
+                ltAppSettings.Text += string.Format("{0} = {1} </br>", key, ConfigurationManager.AppSettings[key]);
+            }
+        }
+    }
+}
